d957f220e8ea1122621ac8bc1dcb48055575cd1e
nokotan <kamenokonokotan@gmail.com>
2021-05-16 03:34:43 +0900

    Add opusfile.sh (experimental)

---
+++ scripts/opusfile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RepositoryName="opusfile"
+RepositoryAddress="https://github.com/xiph/opusfile.git"
+
+function init() {
+    cd ${RepositoryDir}
+
+    if [ ! -e "${RepositoryName}" ]; then
+        git clone --depth 1 ${RepositoryAddress}      
+    fi
+
+    cd ${RepositoryName}
+    ./autogen.sh
+}
+
+function build_wasm() {
+    rm -rf build_wasm
+    mkdir build_wasm
+    cd build_wasm
+    emconfigure ../configure --prefix=${SysRootDir}
+    make
+    make install
+}
+
+function build_wasm_pic() {
+    rm -rf build_wasm_pic
+    mkdir build_wasm_pic
+    cd build_wasm_pic
+    emconfigure ../configure --prefix=${SysRootDir} CFLAGS='-fPIC' CXXFLAGS='-fPIC'
+    make 
+    make install
+}
+
+function build_asmjs() {
+    rm -rf build_asmjs
+    mkdir build_asmjs
+    cd build_asmjs
+    emconfigure ../configure --prefix=${SysRootDir}
+    make
+    make install
+}
