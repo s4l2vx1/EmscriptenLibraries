24713bd3dc47aad96d634b9034064dae878af4b6
kamenokonokotan <kamenokonokotan@gmail.com>
2021-05-18 03:22:37 +0900

    update opusfile.sh

--- scripts/opusfile.sh
+++ scripts/opusfile.sh
@@ -18,8 +18,7 @@ function build_wasm() {
     rm -rf build_wasm
     mkdir build_wasm
     cd build_wasm
-    emconfigure ../configure --prefix=${SysRootDir}
-    make
+    emconfigure ../configure --prefix=${SysRootDir} --disable-shared --enable-static --disable-examples
     make install
 }
 
@@ -27,8 +26,7 @@ function build_wasm_pic() {
     rm -rf build_wasm_pic
     mkdir build_wasm_pic
     cd build_wasm_pic
-    emconfigure ../configure --prefix=${SysRootDir} CFLAGS='-fPIC' CXXFLAGS='-fPIC'
-    make 
+    emconfigure ../configure --prefix=${SysRootDir} CFLAGS='-fPIC' CXXFLAGS='-fPIC' --disable-shared --enable-static --disable-examples
     make install
 }
 
@@ -36,7 +34,6 @@ function build_asmjs() {
     rm -rf build_asmjs
     mkdir build_asmjs
     cd build_asmjs
-    emconfigure ../configure --prefix=${SysRootDir}
-    make
+    emconfigure ../configure --prefix=${SysRootDir} --disable-shared --enable-static --disable-examples
     make install
 }
